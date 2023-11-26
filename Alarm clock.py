23b55e91c68b13003a7ec378389ab6d8567748ae
Lin-Calin <151988007+Lin-Calin@users.noreply.github.com>
2023-11-26 20:17:59 +0200

    Add files via upload

---
+++ Alarm clock.py
@@ -0,0 +1,16 @@
+import datetime
+
+alarmHour = int(input("Enter the hour: "))
+alarmMinute = int(input("Enter the minutes: "))
+ampm = str(input("am or pm"))
+
+if (ampm == "pm"):
+    alarmHour = alarmHour + 12
+
+while (1 == 1):
+    if (alarmHour == datetime.datetime.now().hour and
+            alarmMinute == datetime.datetime.now().minute):
+        print('IT IS TIME TO WAKE UP \U0001F62D')
+        break
+print("iesire")
+
